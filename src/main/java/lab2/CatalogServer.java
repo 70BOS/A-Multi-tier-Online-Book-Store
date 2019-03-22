b6f2657860d20a003283ef4910f2675f7825f5fa
70BOS <whnn97@gmail.com>
2019-03-22 15:19:32 +0800

    return json object

--- src/main/java/lab2/CatalogServer.java
+++ src/main/java/lab2/CatalogServer.java
@@ -1,6 +1,7 @@
 package lab2;
 
 import static spark.Spark.*;
+import static lab2.JsonUtil.*;
 import java.util.*;
 
 public class CatalogServer {
@@ -24,24 +25,43 @@ public class CatalogServer {
 			String param = req.queryParams("id");
 			int id = Integer.parseInt(param);
 			Book b = query(id);
-			return "Price="+b.getPrice()+"&Quantity="+b.getQuantity();
-		});
+			
+			Map<String,Object> result = new HashMap<String,Object>();//Object!!!!!
+			if(b!=null) {
+				result.put("quantity", b.getQuantity());
+				result.put("price", b.getPrice());
+			}
+			return result;
+		},json());
 		
 		//query by topic
-		//IP:3154/queryTopic?topic=Game
+		//try IP:3154/queryTopic?topic=Game
 		get("/queryTopic",(req, res) ->{
 			String topic = req.queryParams("topic");
 			List<Book> searchResult = query(topic);
 			
-			String s = "";
+			Map<String,Object> result = new HashMap<String,Object>();//Object!!!!!
 			for(Book b:searchResult) {
-				s+=b.getTitle()+"\n";
+				result.put(b.getTitle(), b.getItemNumber());
 			}
-			
-			return s;
-		});
+			return result;
+		},json());
 		
-		put("/update",(req, res) -> "Hello World");
+		put("/update",(req, res) -> {
+			String param = req.queryParams("id");
+			int id = Integer.parseInt(param);
+			if(update(id)) {
+				return "book sold";
+			}
+			else {
+				return "????????????????";
+			}
+		},json());
+		
+		//changes every response to application/json
+		after((req, res) -> {
+			  res.type("application/json");
+			});
 	}
 	
 	//get book by id
@@ -64,8 +84,13 @@ public class CatalogServer {
 	}
 	
 	//sell book by id,quantity--
-	public void update(int id) {
+	public boolean update(int id) {
 		Book b = query(id);
-		b.sell();
+		if(b.getQuantity()>0) {
+			b.sell();
+			System.out.print("Catalog Server sold book "+b.getTitle()+" and "+b.getQuantity()+"in stock");
+			return true;
+		}
+		return false;
 	}
 }
