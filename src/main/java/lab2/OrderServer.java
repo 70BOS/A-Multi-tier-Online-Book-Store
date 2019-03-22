c5ea390f0dab774f763666936e727d25766a3add
70BOS <whnn97@gmail.com>
2019-03-22 16:53:13 +0800

    order server

---
+++ src/main/java/lab2/OrderServer.java
@@ -0,0 +1,32 @@
+package lab2;
+
+import static spark.Spark.*;
+import static lab2.JsonUtil.*;
+import static lab2.HttpUtil.*;
+import java.util.*;
+
+
+public class OrderServer {
+	
+	//TODO order log??????
+	
+	public void start() {
+		port(3800);
+		
+		get("/buy",(req, res) ->{
+			//TODO
+			Response queryResponse = request("GET","http://localhost:3154/queryItem?id=1");
+			Map<String,Object> result = queryResponse.json();
+			if((int)result.get("quantity")>0) {
+				Response buyResponse = request("PUT","http://localhost:3154/update?id=1");
+				return buyResponse.status;
+			}
+			return null;
+		},json());
+		
+		after((req, res) -> {
+			res.type("application/json");
+		});
+	}
+	
+}
